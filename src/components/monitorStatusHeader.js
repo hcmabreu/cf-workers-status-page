0eb851682089e2f19e84a06d46a6db2ffdf55f93
Henrique  Abreu <henrique.abreu@angioclin.med.br>
2024-04-07 11:07:37 -0300

    Update monitorStatusHeader.js

--- src/components/monitorStatusHeader.js
+++ src/components/monitorStatusHeader.js
@@ -23,9 +23,9 @@ export default function MonitorStatusHeader({ kvMonitorsLastUpdate }) {
         <div>{text}</div>
         {kvMonitorsLastUpdate.time && typeof window !== 'undefined' && (
           <div className="text-xs font-light">
-            checked{' '}
-            {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} sec
-            ago (from{' '}
+            verificado{' '}
+            {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} segundos
+            atrás (a partir de {' '}
             {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc})
           </div>
         )}
